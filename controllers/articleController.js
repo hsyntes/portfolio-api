0b534eaccd953376a525913088d3c7617011fcff
hsyntes <se.hsyntes@gmail.com>
2023-11-08 22:54:48 +0200

    update & create article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -65,51 +65,51 @@ exports.getArticle = async (req, res, next) => {
   }
 };
 
-exports.createArticle = async (req, res, next) => {
-  try {
-    const article = await Article.create({
-      article_title: req.body.title,
-      article_description: req.body.description,
-      article_thumbnail: req.body.thumbnail,
-      article_documentation: req.body.documentation,
-    });
+// exports.createArticle = async (req, res, next) => {
+//   try {
+//     const article = await Article.create({
+//       article_title: req.body.title,
+//       article_description: req.body.description,
+//       article_thumbnail: req.body.thumbnail,
+//       article_documentation: req.body.documentation,
+//     });
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        article,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
-};
+//     res.status(201).json({
+//       status: "success",
+//       data: {
+//         article,
+//       },
+//     });
+//   } catch (e) {
+//     next(e);
+//   }
+// };
 
-exports.updateArticle = async (req, res, next) => {
-  try {
-    if (!req.params.id)
-      return next(new ErrorProvider(403, "fail", "An article id is required."));
+// exports.updateArticle = async (req, res, next) => {
+//   try {
+//     if (!req.params.id)
+//       return next(new ErrorProvider(403, "fail", "An article id is required."));
 
-    const { id } = req.params;
+//     const { id } = req.params;
 
-    const article = await Article.findByIdAndUpdate(
-      id,
-      {
-        article_title: req.body.title,
-        article_description: req.body.description,
-        article_thumbnail: req.body.thumbnail,
-        article_documentation: req.body.documentation,
-      },
-      { new: true, runValidators: true }
-    );
+//     const article = await Article.findByIdAndUpdate(
+//       id,
+//       {
+//         article_title: req.body.title,
+//         article_description: req.body.description,
+//         article_thumbnail: req.body.thumbnail,
+//         article_documentation: req.body.documentation,
+//       },
+//       { new: true, runValidators: true }
+//     );
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        article,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
-};
+//     res.status(200).json({
+//       status: "success",
+//       data: {
+//         article,
+//       },
+//     });
+//   } catch (e) {
+//     next(e);
+//   }
+// };
