d51fa21b64222f660c23c7ad50dcb99d30b9b6d6
hsyntes <se.hsyntes@gmail.com>
2023-11-28 23:29:45 +0200

    update CORS configuration

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(compression());
 
 // * Cors configuration
 const allowedOrigins = [
-  "http://localhost:8000",
+  "http://localhost:3000",
   "https://www.hsyntes.com",
   "https://hsyntes.com",
   "https://hsyntes.vercel.app",
